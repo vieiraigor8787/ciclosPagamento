1e30b3705590eecaf53b1e882f5a177e03da8a81
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-22 18:57:24 -0300

    form: create() in actions and import om billcyclo

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -8,3 +8,11 @@ export function getList(){
         payload: req
     }
 }
+
+export function create(values) {
+    // console.log(values)
+    axios.post(`${BASE_URL}/billingCycles`, values)
+    return {
+        type: 'TEMP'
+    }
+}
