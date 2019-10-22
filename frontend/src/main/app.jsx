741d2963122561f75bc232b493f3bf05110111a2
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-21 21:23:18 -0300

    menu , sidebar

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import '../common/template/dependences';
 
 import Header from '../common/template/header';
+import Sidebar from '../common/template/sidebar';
+
 
 export default props => (
     <div className="wrapper">
         <Header />
+        <Sidebar />
     </div>
 )
