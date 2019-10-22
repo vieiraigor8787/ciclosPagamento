2ec19b96bb0c71ea300d6187399dbaa38bacb879
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-22 18:28:55 -0300

    create billingCycleForm, import in billingCycle

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -11,12 +11,13 @@ import TabHeader from '../common/tab/tabHeader';
 import TabContent from '../common/tab/tabContent';
 import { selectTab, showTabs } from '../common/tab/tabActions';
 import List from './billingCycleList';
+import Form from './billingCycleForm';
 
 class BillingCycle extends Component {
 
     componentWillMount() {
         this.props.selectTab('tabList')
-        this.props.showTabs('tabList', 'tabUpdate')
+        this.props.showTabs('tabList', 'tabCreate')
     }
 
     render() {
@@ -36,7 +37,7 @@ class BillingCycle extends Component {
                        <List />
                     </TabContent>
                     <TabContent id='tabCreate'>
-                        <h1>Incluir</h1>
+                        <Form />
                     </TabContent>
                     <TabContent id='tabUpdate'>
                         <h1>Alterar</h1>
