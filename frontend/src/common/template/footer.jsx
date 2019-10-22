c7327c457444e93319c1ee3be9d59f75b40c0e78
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-21 21:34:38 -0300

    footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2017
+            <a href="google.com.br" target="_blank"> Goo</a>
+        </strong>
+    </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,11 +3,15 @@ import '../common/template/dependences';
 
 import Header from '../common/template/header';
 import Sidebar from '../common/template/sidebar';
-
+import Footer from '../common/template/footer';
 
 export default props => (
     <div className="wrapper">
         <Header />
         <Sidebar />
+        <div className="content-wrapper">
+            <h1>content</h1>
+        </div>
+        <Footer />
     </div>
 )
