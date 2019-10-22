ea697cb18d889b57ec91993edf88c6b2d6926884
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-22 11:34:52 -0300

    ajustes layout

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
+import Vb from '../common/widget/valueBox';
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render() {
@@ -9,7 +11,29 @@ class Dashboard extends Component {
             <div>
               <ContentHeader title='Dashboard' small='Versao 1.0' />
               <Content>
-                  Dashboard
+                <Row>
+                    <Vb 
+                    cols='12 4' 
+                    color='green' 
+                    icon='bank'
+                    value='R$10'
+                    text='Total crédito' 
+                    />
+                    <Vb 
+                    cols='12 4' 
+                    color='red' 
+                    icon='credit-card'
+                    value='R$10'
+                    text='Total débito' 
+                    />
+                    <Vb 
+                    cols='12 4' 
+                    color='blue' 
+                    icon='money'
+                    value='R$0'
+                    text='valor consolidado' 
+                    />
+                </Row>
               </Content>
             </div>
         )
