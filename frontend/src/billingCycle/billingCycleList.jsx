928ed9f1ae45dd27f3aaf76252587c06e544d40d
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-22 18:20:35 -0300

    billingcycle: renderRows(), list map

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -8,6 +8,18 @@ class BillingCycleList extends Component {
     componentWillMount() {
         this.props.getList()
     }
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+              <td>{bc.name}</td>
+              <td>{bc.month}</td>
+              <td>{bc.year}</td>
+            </tr>
+        ))
+    }
+
     render(){
         // console.log(this.props.list)
         return (
@@ -21,7 +33,7 @@ class BillingCycleList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
