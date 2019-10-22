0b540353cde50dfd337d31912dd203d19ade3bd6
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-22 12:47:50 -0300

    dashboard: mapStateToProps > credit/debt

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'redux';
 
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
@@ -7,37 +8,42 @@ import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render() {
-        return (
-            <div>
-              <ContentHeader title='Dashboard' small='Versao 1.0' />
-              <Content>
-                <Row>
-                    <Vb 
-                    cols='12 4' 
-                    color='green' 
-                    icon='bank'
-                    value='R$10'
-                    text='Total crédito' 
-                    />
-                    <Vb 
-                    cols='12 4' 
-                    color='red' 
-                    icon='credit-card'
-                    value='R$10'
-                    text='Total débito' 
-                    />
-                    <Vb 
-                    cols='12 4' 
-                    color='blue' 
-                    icon='money'
-                    value='R$0'
-                    text='valor consolidado' 
-                    />
-                </Row>
-              </Content>
-            </div>
-        )
+      const { credit, debt } = this.props.summary
+      return (
+          <div>
+            <ContentHeader title='Dashboard' small='Versao 1.0' />
+            <Content>
+              <Row>
+                  <Vb 
+                  cols='12 4' 
+                  color='green' 
+                  icon='bank'
+                  value={`R$ ${credit}`}
+                  text='Total crédito' 
+                  />
+                  <Vb 
+                  cols='12 4' 
+                  color='red' 
+                  icon='credit-card'
+                  value={`R$ ${debt}`}
+                  text='Total débito' 
+                  />
+                  <Vb 
+                  cols='12 4' 
+                  color='blue' 
+                  icon='money'
+                  value={`R$ ${credit - debt}`}
+                  text='valor consolidado' 
+                  />
+              </Row>
+            </Content>
+          </div>
+      )
     }
 }
 
+const mapStateToProps = state => ({
+  summary: state.dashboard.summary
+});
+export default connect(mapStateToProps)(Dashboard)
 export default Dashboard
