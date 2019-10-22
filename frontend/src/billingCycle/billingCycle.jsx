21852bc408f80073514f3f390173984bf35ee108
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-22 16:48:33 -0300

    billingcycle: showtabs(), reducer

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -9,12 +9,13 @@ import TabsHeader from '../common/tab/tabsHeader';
 import TabsContent from '../common/tab/tabsContent';
 import TabHeader from '../common/tab/tabHeader';
 import TabContent from '../common/tab/tabContent';
-import { selectTab } from '../common/tab/tabActions';
+import { selectTab, showTabs } from '../common/tab/tabActions';
 
 class BillingCycle extends Component {
 
     componentWillMount() {
         this.props.selectTab('tabList')
+        this.props.showTabs('tablist', 'tabCreate')
     }
 
     render() {
@@ -51,5 +52,5 @@ class BillingCycle extends Component {
 }
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({selectTab}, dispatch);
+  bindActionCreators({selectTab, showTabs}, dispatch);
 export default connect(null, mapDispatchToProps)(BillingCycle)
