ff5deaec97cff6009b9697cece9d15a5e29e474c
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-26 17:23:57 -0300

    updt cors: Authorization / updt component routes

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,29 @@
-const express = require('express')
+const express = require('express');
+const auth = require('./auth');
 
 module.exports = function(server) {
 
-    // Definir URL base para todas as rotas 
-    const router = express.Router()
-    server.use('/api', router)
+     /*
+      * Rotas protegidas por token JWT
+      */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
 
     // Rotas de Ciclo de Pagamento 
     const BillingCycle = require('../api/billingCycle/billingCycleService')
     BillingCycle.register(router, '/billingCycles')
+
+    /*
+     * Rotas abertas
+     */
+    const openApi = express.Router()
+    server.use('/opai', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+ 
 }
